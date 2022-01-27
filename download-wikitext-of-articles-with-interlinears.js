71eaf7d53a9673d96b740d943590f01aa7947e65
Patrick Hall <pjh@umail.ucsb.edu>
2022-01-27 11:05:31 -0500

    Fun to check in a few hundred interlinear files!

--- download-wikitext-of-articles-with-interlinears.js
+++ download-wikitext-of-articles-with-interlinears.js
@@ -2,9 +2,10 @@ import articles from "./articles-with-interlinears.json" assert { type: "json" }
 import {spiderWikitext} from './spider-wikitext.js'
 import {extractInterlinears} from './extract-interlinears-from-wikitext.js'
 
-for await (let {title,url} of articles.slice(0,3)){
+for await (let {title,url} of articles.slice(134)){
   let wikitext = await spiderWikitext(title)
   let interlinears = extractInterlinears(wikitext)
 
 
+  Deno.writeTextFileSync(`./samples/${title.replaceAll(' ', '_')}.txt`, interlinears)
 }
